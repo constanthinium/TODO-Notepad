49828f721306433bae5c19d4c46972aece1f3000
constanthinium <constanthinium@gmail.com>
2020-12-05 00:57:00 +0300

    Add project files.

---
+++ TODO Notepad/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using System.Windows.Threading;
+
+namespace TODO_Notepad
+{
+    public partial class App : Application
+    {
+        private void Application_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show("Exception: " + e.Exception.Message, "Exception", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+    }
+}
---
+++ TODO Notepad/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System.Windows.Media;
+using System.Windows;
+using System.Windows.Controls;
+using System.ComponentModel;
+using System.IO;
+
+namespace TODO_Notepad
+{
+    public partial class MainWindow : Window
+    {
+        private const string path = "todo.bin";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            if (File.Exists(path))
+                using (BinaryReader reader = new BinaryReader(File.OpenRead(path)))
+                {
+                    textBox.Text = reader.ReadString();
+                    textBox.CaretIndex = reader.ReadInt32();
+
+                    Left = reader.ReadInt32();
+                    Top = reader.ReadInt32();
+                    Width = reader.ReadInt32();
+                    Height = reader.ReadInt32();
+                }
+        }
+
+        private void TopmostMenuItem_CheckedChanged(object sender, RoutedEventArgs e)
+        {
+            Topmost = ((MenuItem)sender).IsChecked;
+        }
+
+        private void MonospaceMenuItem_Checked(object sender, RoutedEventArgs e)
+        {
+            if (textBox != null)
+                textBox.FontFamily = new FontFamily(((MenuItem)sender).IsChecked ? "Consolas" : "Segoe UI");
+        }
+
+        private void Window_Closing(object sender, CancelEventArgs e)
+        {
+            using (BinaryWriter writer = new BinaryWriter(File.OpenWrite(path)))
+            {
+                writer.Write(textBox.Text);
+                writer.Write(textBox.CaretIndex);
+
+                writer.Write((int)Left);
+                writer.Write((int)Top);
+                writer.Write((int)Width);
+                writer.Write((int)Height);
+            }
+        }
+    }
+}
